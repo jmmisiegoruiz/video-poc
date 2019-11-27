41c248644547c930e2f9ca869b7412bc479052d1
Jose Matias Misiego Ruiz <josematias.misiego@kbc.be>
2019-11-27 13:35:53 +0100

    just a video poc to test using camera in browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'video-poc';
+
+  @ViewChild('videoElement', {static: true}) videoElement: any;
+  video: any;
+
+  initCamera(config: any) {
+    const browser: any = navigator;
+
+    browser.getUserMedia = (browser.getUserMedia ||
+      browser.webkitGetUserMedia ||
+      browser.mozGetUserMedia ||
+      browser.msGetUserMedia);
+
+    browser.mediaDevices.getUserMedia(config).then(stream => {
+      this.video.srcObject = stream;
+      this.video.play();
+    });
+  }
+
+  ngOnInit() {
+    this.video = this.videoElement.nativeElement;
+    this.initCamera({video: true, audio: false});
+  }
 }
